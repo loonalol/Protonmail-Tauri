2595adc377fc51afe0cd98f6b317894f5e1c5e79
Fluffy <flufsoldu@proton.me>
2023-10-03 00:34:54 -0500

    added app

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::Manager;
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  args: Vec<String>,
+  cwd: String,
+}
+
+
+fn main() {
+    tauri::Builder::default()
+
+    .plugin(tauri_plugin_positioner::init())
+    .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+        println!("{}, {argv:?}, {cwd}", app.package_info().name);
+        app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
+    }))
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
